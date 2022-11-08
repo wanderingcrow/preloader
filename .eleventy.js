0d1d77b6a17454b59e6f8fa38456e1ab21ef8c41
Perry Kibler <perry@wanderingcrow.design>
2022-11-08 12:46:10 -0800

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,55 @@
+const sass = require("sass");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["avif", "jpeg"]
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+
+  // Copy `/src/asset/images` to `_site/assets/images`
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+  
+  eleventyConfig.addTemplateFormats("scss");
+
+  // Creates the extension for use
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css", // optional, default: "html"
+
+    // `compile` is called once per .scss file in the input directory
+    compile: async function(inputContent) {
+      let result = sass.compileString(inputContent);
+
+      // This is the render function, `data` is the full data cascade
+      return async (data) => {
+        return result.css;
+      };
+    }
+  });
+
+  eleventyConfig.addWatchTarget("./src/sass/");
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+};
+
+
